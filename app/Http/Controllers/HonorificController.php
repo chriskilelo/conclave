beaee3f4e971537b2732cc2300029ddd3fb03118
Chris Kilelo <chriskilelo@gmail.com>
2025-07-07 05:41:46 +0300

    chore: create default files for honorifics module
    - the fresh files below were created
       - app/Models/Honorific.php
       - database/factories/HonorificFactory.php
       - database/migrations/2025_07_07_023648_create_honorifics_table.php
       - database/seeders/HonorificSeeder.php
       - app/Http/Requests/StoreHonorificRequest.php
       - app/Http/Requests/UpdateHonorificRequest.php
       - app/Http/Controllers/HonorificController.php
       - app/Policies/HonorificPolicy.php
       - resources/js/pages/honorifics/Index.vue
       - resources/js/pages/honorifics/Create.vue
       - resources/js/pages/honorifics/Edit.vue

---
+++ app/Http/Controllers/HonorificController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreHonorificRequest;
+use App\Http\Requests\UpdateHonorificRequest;
+use App\Models\Honorific;
+
+class HonorificController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreHonorificRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Honorific $honorific)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Honorific $honorific)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateHonorificRequest $request, Honorific $honorific)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Honorific $honorific)
+    {
+        //
+    }
+}
