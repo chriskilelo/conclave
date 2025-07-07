237a11f3356f44b38347279eabb922f0f0d17fd0
Chris Kilelo <chriskilelo@gmail.com>
2025-07-07 08:13:25 +0300

    chore(seeder): call HonorificSeeder from DatabaseSeeder
       - Ensures that the honorifics are seeded when running the main seeder.
       - This will provide the data required on the 'honorifics' dropdown list

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,11 +13,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // Call the HonorificSeeder to populate the honorifics table
+         $this->call(HonorificSeeder::class);
     }
 }
