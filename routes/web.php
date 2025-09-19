c34013c888ae886cde07682d32a2796283a381cc
Chris Kilelo <chriskilelo@gmail.com>
2025-09-19 23:56:10 +0300

    add routes required for Honorifics module CRUD ops

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\HonorificController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
@@ -11,5 +12,17 @@ Route::get('dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
-require __DIR__.'/settings.php';
-require __DIR__.'/auth.php';
+// protect these routes with the authentication middleware
+Route::middleware('auth')->group(function () {
+    // Honorific Routes
+    Route::get('honorifics', [HonorificController::class, 'index'])->name('honorifics.index');
+    Route::get('honorifics/create', [HonorificController::class, 'create'])->name('honorifics.create');
+    Route::post('honorifics', [HonorificController::class, 'store'])->name('honorifics.store');
+    Route::get('honorifics/{honorific}', [HonorificController::class, 'show'])->name('honorifics.show');
+    Route::get('honorifics/{honorific}/edit', [HonorificController::class, 'edit'])->name('honorifics.edit');
+    Route::put('honorifics/{honorific}', [HonorificController::class, 'update'])->name('honorifics.update');
+    Route::delete('honorifics/{honorific}', [HonorificController::class, 'destroy'])->name('honorifics.destroy');
+});
+
+require __DIR__ . '/settings.php';
+require __DIR__ . '/auth.php';
